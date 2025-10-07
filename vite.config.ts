fc31bdb2d37d46bc3c414f703df4a96d99594372
r4ppz <johnreyrabosa.f@gmail.com>
2025-10-07 15:11:10 +0800

    feat: add path alias "@" for src directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
       "Cross-Origin-Embedder-Policy": "unsafe-none",
       "Cross-Origin-Resource-Policy": "cross-origin",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
