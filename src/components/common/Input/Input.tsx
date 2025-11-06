f523827f548675ae26bcdc74a0af2235c3210b79
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-06 19:31:12 +0800

    refactor: convert Input component to use forwardRef

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { type ComponentType, type InputHTMLAttributes, type Ref } from "react";
+import { type ComponentType, type InputHTMLAttributes, forwardRef } from "react";
 import style from "./Input.module.css";
 
 type InputType =
@@ -17,16 +17,17 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: InputType;
   icon?: ComponentType<{ className?: string }>;
   className?: string;
-  ref?: Ref<HTMLInputElement>;
 }
 
-function Input({ type = "text", icon: Icon, className, ref, ...props }: InputProps) {
-  return (
-    <div className={clsx(style.inputWrapper, className)}>
-      {Icon && <Icon className={style.icon} />}
-      <input {...props} ref={ref} type={type} className={style.input} />
-    </div>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type = "text", icon: Icon, className, ...props }, ref) => {
+    return (
+      <div className={clsx(style.inputWrapper, className)}>
+        {Icon && <Icon className={style.icon} />}
+        <input ref={ref} type={type} className={style.input} {...props} />
+      </div>
+    );
+  },
+);
 
 export default Input;
