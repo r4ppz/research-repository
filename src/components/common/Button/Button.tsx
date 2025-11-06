8767be7ce9c2719f7ec8ccbfd0857d31dc020193
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-06 19:19:27 +0800

    refactor(Button): enable ref forwarding

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,19 +1,21 @@
 import clsx from "clsx";
-import { type ButtonHTMLAttributes } from "react";
+import { type ButtonHTMLAttributes, forwardRef } from "react";
 import style from "./Button.module.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary";
 }
 
-function Button({ variant = "primary", children, className, ...props }: ButtonProps) {
-  const buttonClass = clsx(style.button, style[variant], className);
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant = "primary", children, className, type = "button", ...props }, ref) => {
+    const buttonClass = clsx(style.button, style[variant], className);
 
-  return (
-    <button type="button" className={buttonClass} {...props}>
-      {children}
-    </button>
-  );
-}
+    return (
+      <button ref={ref} type={type} className={buttonClass} {...props}>
+        {children}
+      </button>
+    );
+  },
+);
 
 export default Button;
