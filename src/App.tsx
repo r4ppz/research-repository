756f3810d514b218d42a207f8bb93e080f98455c
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-12 17:27:46 +0800

    refractor(ProtectedRoute): rename ProtectedRount dir to Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import ProtectedRoute from "@/components/common/ProtectedRoute/ProtectedRoute";
+import ProtectedRoute from "@/components/common/Route/ProtectedRoute";
 import DepartmentAdminRequestsPage from "@/features/admin/department/RequestPage/RequestPage";
 import DepartmentAdminResearchPage from "@/features/admin/department/ResearchPage/ResearchPage";
 import SuperAdminRequestsPage from "@/features/admin/super/RequestPage/RequestPage";
