23d935f291d9695ab522c0e7d9c8b11b061041db
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-15 19:58:54 +0800

    fix(Textarea): wrong way of using ref
    - use forwardRef same as Input

--- src/components/common/Textarea/Textarea.tsx
+++ src/components/common/Textarea/Textarea.tsx
@@ -1,20 +1,21 @@
 import clsx from "clsx";
-import { type ComponentType, type TextareaHTMLAttributes, type Ref } from "react";
+import { type ComponentType, type TextareaHTMLAttributes, forwardRef } from "react";
 import style from "./Textarea.module.css";
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string;
-  ref?: Ref<HTMLTextAreaElement>;
   icon?: ComponentType<{ className?: string }>;
 }
 
-const Textarea = ({ className, ref, icon: Icon, ...props }: TextareaProps) => {
-  return (
-    <div className={clsx(style.textareaContainer, className)}>
-      {Icon && <Icon className={style.icon} />}
-      <textarea {...props} ref={ref} className={style.textarea} />
-    </div>
-  );
-};
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, icon: Icon, ...props }, ref) => {
+    return (
+      <div className={clsx(style.textareaContainer, className)}>
+        {Icon && <Icon className={style.icon} />}
+        <textarea {...props} ref={ref} className={style.textarea} />
+      </div>
+    );
+  },
+);
 
 export default Textarea;
