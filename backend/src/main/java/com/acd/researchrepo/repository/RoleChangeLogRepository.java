1bd8b64a73ea2bfbfaed27dbcc553a627dad8cc2
Win <erwincurato@gmail.com>
2026-07-02 14:57:50 +0800

    feat: create admin role management endpoints

---
+++ backend/src/main/java/com/acd/researchrepo/repository/RoleChangeLogRepository.java
@@ -0,0 +1,6 @@
+package com.acd.researchrepo.repository;
+
+import com.acd.researchrepo.model.RoleChangeLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleChangeLogRepository extends JpaRepository<RoleChangeLog, Integer> {}
