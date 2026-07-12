f622e596ac66c06de1d709a09e1efa66cd6e79cc
Win <erwincurato@gmail.com>
2026-07-12 11:51:50 +0800

    refactor(frontend):migrate Select to React Aria

--- frontend/src/components/common/Select/Select.tsx
+++ frontend/src/components/common/Select/Select.tsx
@@ -1,24 +1,22 @@
 import { ChevronDown } from "lucide-react";
-import React from "react";
 import {
   Select as AriaSelect,
-  SelectProps as AriaSelectProps,
-  ListBoxItemProps,
-  ListBoxProps,
+  type SelectProps as AriaSelectProps,
+  Button,
+  ListBox,
+  ListBoxItem,
+  type ListBoxItemProps,
+  Popover,
   SelectValue,
-  ValidationResult,
+  type ValidationResult,
 } from "react-aria-components";
-import { Button } from "../Button/Button";
-import { DropdownItem, DropdownListBox } from "../DropdownList/DropdownList";
 import { Description, FieldError, Label } from "../Form/Form";
-import { Popover } from "../Popover/Popover";
 import styles from "./Select.module.css";
 
 export interface SelectProps<T extends object> extends Omit<AriaSelectProps<T>, "children"> {
   label?: string;
   description?: string;
   errorMessage?: string | ((validation: ValidationResult) => string);
-  items?: Iterable<T>;
   children: React.ReactNode | ((item: T) => React.ReactNode);
 }
 
@@ -27,33 +25,30 @@ export function Select<T extends object>({
   description,
   errorMessage,
   children,
-  items,
   ...props
 }: SelectProps<T>) {
   return (
-    <AriaSelect {...props} className={styles.select}>
-      {label && <Label>{label}</Label>}
+    <div className={styles.select}>
+      <AriaSelect {...props}>
+        {label && <Label>{label}</Label>}
 
-      <Button className={styles.button}>
-        <SelectValue className={styles.selectValue} />
-        <ChevronDown className={styles.chevron} />
-      </Button>
+        <Button className={styles.trigger}>
+          <SelectValue className={styles.selectValue} />
+          <ChevronDown className={styles.chevron} aria-hidden="true" />
+        </Button>
 
-      {description && <Description>{description}</Description>}
+        {description && <Description>{description}</Description>}
+        <FieldError>{errorMessage}</FieldError>
 
-      <FieldError>{errorMessage}</FieldError>
-
-      <Popover hideArrow className={styles.selectPopover}>
-        <SelectListBox items={items}>{children}</SelectListBox>
-      </Popover>
-    </AriaSelect>
+        <Popover className={styles.popover} offset={4}>
+          <ListBox className={styles.listBox} shouldFocusWrap>
+            {children}
+          </ListBox>
+        </Popover>
+      </AriaSelect>
+    </div>
   );
 }
 
-export function SelectListBox<T extends object>(props: ListBoxProps<T>) {
-  return <DropdownListBox {...props} />;
-}
-
-export function SelectItem(props: ListBoxItemProps) {
-  return <DropdownItem {...props} />;
-}
+export type { ListBoxItemProps as SelectItemProps };
+export const SelectItem = ListBoxItem;
